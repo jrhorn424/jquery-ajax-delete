90a572d832d5597c0e7df0b1f17e31386c54a12e
Jeffrey Horn <jeffh@generalassemb.ly>
2017-01-20 11:24:55 -0500

    Attach delete handler

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -18,4 +18,5 @@ const bookEvents = require('./books/events');
 // On document ready
 $(() => {
   $('#book-search').on('submit', bookEvents.onGetBooks);
+  $('#book-destroy').on('submit', bookEvents.onDeleteBook);
 });
